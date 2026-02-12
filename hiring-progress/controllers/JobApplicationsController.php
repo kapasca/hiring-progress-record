c0c6320f7bd37921e1691539b430a50546ba641a
Kapasca <kapasca@techgrid.co>
2026-02-12 19:36:27 +0700

    Init controller and view

---
+++ hiring-progress/controllers/JobApplicationsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class JobApplicationsController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
